0348c92265a69ab6340d96a44fde04cafd363aa4
xdlimao <danieloliveiradesouza09@outlook.com>
2023-11-06 19:24:10 -0300

    circulo está completo
    funcional, só falta adicionar os elementos na tela

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,19 @@
-function oi(){
-alert("oi")
+const content = document.getElementById("contentdiv")
+
+// Usar ' (apóstrofo) para passar argumentos no HTML e " (aspas) para passar atributos HTML
+
+function quemsoueu(){
+    content.innerHTML = '<p class="text-center">wow!</p>'
+}
+
+function frontend(){
+    alert("Frontend")
+}
+
+function projetos(){
+    alert("Projetos")
+}
+
+function backend(){
+    alert("Backend")
 }
