67faeb96daded8dd70b59ed99ff9885d4eca4721
xdlimao <danieloliveiradesouza09@outlook.com>
2023-11-06 21:55:25 -0300

    opções backend criado

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,17 +3,17 @@ const content = document.getElementById("contentdiv")
 // Usar ' (apóstrofo) para passar argumentos no HTML e " (aspas) para passar atributos HTML
 
 function quemsoueu(){
-    content.innerHTML = '<p class="text-center">wow!</p>'
+    content.innerHTML = '<p class="text-center">xdeu</p>'
 }
 
 function frontend(){
-    content.innerHTML = '<ul class="list-group list-group-flush"><li class="list-group-item"><img src="assets/images/html5_css3_logo.png"id="htmlcss"> <h4 class="text-center">HTML + CSS</h4> </li> <li class="list-group-item"> <img src="assets/images/js_logo.png" id="js"> <h4 class="text-center">JS</h4> </li> <li class="list-group-item"> <img src="assets/images/bootstrap_logo.png" id="bootstrap"> <h4 class="text-center">Bootstrap</h4> </li> <li class="list-group-item"> <img src="assets/images/angularts_logo.png" id="angularts"> <h4 class="text-center">Angular + TS</h4> </li> <li class="list-group-item"> <img src="assets/images/sass_logo.png" id="sass"> <h4 class="text-center">Sass / Scss</h4> </li> </ul>'
+    content.innerHTML = '<ul class="list-group list-group-flush"> <li class="list-group-item"> <img src="assets/images/html5_css3_logo.png" id="htmlcss"> <h4 class="text-center">HTML + CSS</h4> </li> <li class="list-group-item"> <img src="assets/images/js_logo.png" id="js"> <h4 class="text-center">JS</h4> </li> <li class="list-group-item"> <img src="assets/images/bootstrap_logo.png" id="bootstrap"> <h4 class="text-center">Bootstrap</h4> </li> <li class="list-group-item"> <img src="assets/images/angularts_logo.png" id="angularts"> <h4 class="text-center">Angular + TS</h4> </li> <li class="list-group-item"> <img src="assets/images/sass_logo.png" id="sass"> <h4 class="text-center">Sass / Scss</h4> </li> </ul>'
 }
 
 function projetos(){
-
+    content.innerHTML = '<p class="text-center">xdgh</p>'
 }
 
 function backend(){
-
+    content.innerHTML = '<ul class="list-group list-group-flush" style="margin-top: 3px;"> <li class="list-group-item"> <img src="assets/images/spring_logo.png" id="spring"> <h4 class="text-center">Spring</h4> </li> <li class="list-group-item"> <img src="assets/images/androidstudio_logo.png" id="androidstudio"> <h4 class="text-center">Android Studio</h4> </li> <li class="list-group-item"> <img src="assets/images/nodejs_logo.png" id="nodejs"> <h4 class="text-center">Node.js</h4> </li> <li class="list-group-item"> <img src="assets/images/sql_logo.png" id="tsql"> <h4 class="text-center">TSQL</h4> </li> </ul>'
 }
